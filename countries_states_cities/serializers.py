457b0a6ce0017b440bca169882bce04079e66500
Sunwook KIm <sunus7@naver.com>
2023-12-07 16:50:53 +0900

    v0.2.5

--- countries_states_cities/serializers.py
+++ countries_states_cities/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from countries_states_cities.models import Region, Subregion, Country, State, City
 
 
-default_fields = ['id', 'name', 'name_en', 'name_ja', 'name_ko']
+default_fields = ['id', 'name', 'name_en', 'name_ja', 'name_ko', 'wikiDataId']
 
 
 class RegionSerializer(serializers.ModelSerializer):
@@ -21,7 +21,7 @@ class SubregionSerializer(serializers.ModelSerializer):
 class CountrySerializer(serializers.ModelSerializer):
     class Meta:
         model = Country
-        fields = default_fields + ['region', 'subregion']
+        fields = default_fields + ['region', 'subregion', 'iso2', 'currency', 'currency_symbol', 'native', 'emoji']
 
 
 class StateSerializer(serializers.ModelSerializer):
@@ -29,7 +29,7 @@ class StateSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = State
-        fields = default_fields + ['country']
+        fields = default_fields + ['country', 'iso2']
 
 
 class CitySerializer(serializers.ModelSerializer):
